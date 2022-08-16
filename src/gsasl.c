jas4711/gsasl

commit fe54b2f29f6d718462b231602d5dec5952febf05

Fix mem leaks found by new tests.

src/gsasl.c
@@ -911,6 +911,7 @@ main (int argc, char *argv[])
 	return 1;
 
       mech = gsasl_client_suggest_mechanism (ctx, in);
+      free (in);
       if (mech == NULL)
 	{
 	  fprintf (stderr, _("Cannot find mechanism...\n"));
@@ -947,11 +948,13 @@ main (int argc, char *argv[])
 	  int res2;
 
 	  res = gsasl_step64 (xctx, in, &out);
+	  free (in);
 	  if (res != GSASL_NEEDS_MORE && res != GSASL_OK)
 	    break;
 
 	  if (!step_send (out))
 	    return 1;
+	  free (out);
 
 	no_client_first:
 	  if (!args_info.quiet_given &&
@@ -973,7 +976,10 @@ main (int argc, char *argv[])
 	  if (res2 == 3)
 	    error (EXIT_FAILURE, 0, _("server error"));
 	  if (res2 == 2)
-	    break;
+	    {
+	      free (in);
+	      break;
+	    }
 	}
       while (args_info.imap_flag || args_info.smtp_flag
 	     || res == GSASL_NEEDS_MORE);

src/imap.c
@@ -28,6 +28,8 @@ imap_greeting (void)
   if (!readln (&in))
     return 0;
 
+  free (in);
+
   return 1;
 }
 
@@ -45,8 +47,12 @@ imap_has_starttls (void)
 
   has_tls = strstr (in, "STARTTLS") != NULL;
 
+  free (in);
+
   if (!readln (&in))
     return 0;
+
+  free (in);
 
   return has_tls;
 }
@@ -92,6 +98,8 @@ imap_select_mechanism (char **mechlist)
 
       if (!readln (&in))
 	return 0;
+
+      free (in);
     }
 
   return 1;
@@ -164,6 +172,7 @@ imap_step_recv (char **data)
          before the final '. OK'). */
       while (*p == '*')
 	{
+	  free (p);
 	  if (!readln (data))
 	    return 0;
 	  p = *data;

src/smtp.c
@@ -71,7 +71,7 @@ smtp_starttls (void)
 int
 smtp_select_mechanism (char **mechlist)
 {
-  char *in;
+  char *in = NULL;
 
   if (args_info.server_flag)
     {
@@ -86,19 +86,24 @@ smtp_select_mechanism (char **mechlist)
       if (!writeln ("EHLO [127.0.0.1]"))
 	return 0;
 
+      *mechlist = NULL;
       do
 	{
+	  free (in);
 	  if (!readln (&in))
 	    return 0;
 
-#define GREETING1 "250-AUTH "
-#define GREETING2 "250 AUTH "
-	  if (strncmp (in, GREETING1, strlen (GREETING1)) == 0)
-	    *mechlist = in + strlen (GREETING1);
-	  else if (strncmp (in, GREETING2, strlen (GREETING2)) == 0)
-	    *mechlist = in + strlen (GREETING2);
+	  /* Greeting can be '250-AUTH ' or '250 AUTH '. */
+	  if (strlen (in) > 9
+	      && in[0] == '2'
+	      && in[1] == '5'
+	      && in[2] == '0'
+	      && (in[3] == '-' || in[3] == ' ')
+	      && in[4] == 'A'
+	      && in[5] == 'U' && in[6] == 'T' && in[7] == 'H' && in[8] == ' ')
+	    memmove (*mechlist = in, in + 9, strlen (in) - 9);
 	}
-      while (strncmp (in, "250 ", 4) != 0);
+      while (*mechlist == NULL);
     }
 
   return 1;

commit eabdd9dd8773c209b85ef5638c4c435678647f29

Fix problem introduced by fe54b2f29f6d718462b231602d5dec5952febf05.

src/gsasl.c
@@ -249,7 +249,7 @@ select_mechanism (char **mechlist)
     return smtp_select_mechanism (mechlist);
 
   if (args_info.mechanism_arg)
-    *mechlist = args_info.mechanism_arg;
+    *mechlist = xstrdup (args_info.mechanism_arg);
   else if (args_info.server_flag)
     {
       if (!args_info.quiet_given)